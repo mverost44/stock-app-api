c0bcdb4c741ee015e456a68135e521d44da7e286
mverost <mverost4422@gmail.com>
2019-03-16 22:58:37 -0400

    Added calculation of user balance on update trade.

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,4 +1,5 @@
 class CalculationsController < ProtectedController
+  before_action :set_user
   # Calculate total_profit_loss
   def buy_profit_loss(entry_price, exit_price, exit_size)
     total_cost = entry_price.to_i * exit_size.to_i
@@ -6,6 +7,9 @@ class CalculationsController < ProtectedController
     difference = exit_price.to_i - entry_price.to_i
     percent = difference / entry_price
     profit_loss = percent * total_cost
+
+    @user_balance += profit_loss
+    current_user.update(account_balance: @user_balance)
 
     profit_loss
   end
@@ -20,4 +24,10 @@ class CalculationsController < ProtectedController
 
     profit_loss
   end
+end
+
+private
+
+def set_user
+  @user_balance = current_user.account_balance
 end
